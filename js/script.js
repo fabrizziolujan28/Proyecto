127a8880a66f93490d9ef3643ff883fe79b80cdd
fabrizziolujan28 <u25222428@utp.edu.pe>
2025-10-31 13:18:01 -0500

    avance 31/10

--- js/script.js
+++ js/script.js
@@ -11,3 +11,23 @@ document.addEventListener("DOMContentLoaded", () => {
        if (hero) hero.style.backgroundPositionY = offset * 0.5 + "px";
      });
 }
+
+ const counters = document.querySelectorAll(".counter");
+  const animateCounters = () => {
+    counters.forEach(c => {
+      const update = () => {
+        const target = +c.dataset.target;
+        const val = +c.innerText;
+        const inc = target / 80;
+        if (val < target) {
+          c.innerText = Math.ceil(val + inc);
+          setTimeout(update, 40);
+        } else c.innerText = target + "+";
+      };
+      update();
+    });
+  };
+  const obs = new IntersectionObserver(entries => {
+    entries.forEach(e => e.isIntersecting && (animateCounters(), obs.disconnect()));
+  });
+  obs.observe(document.querySelector("#sobre-nosotros"));
