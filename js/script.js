3ba3d8ffc9b9f63a61f324fe25b92fcd2c829142
fabrizziolujan28 <u25222428@utp.edu.pe>
2025-11-08 11:13:20 -0500

    avance 8/11

--- js/script.js
+++ js/script.js
@@ -54,3 +54,4 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("contact-form");
    form?.addEventListener("submit", e => {
       e.preventDefault();
+      const nombre = document.getElementById("nombre").value.trim();
